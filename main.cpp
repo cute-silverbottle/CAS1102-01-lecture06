1e845c580ecd8b87adc56a1266f726310cecbda4
cute-silverbottle <lightsaver9652@gmail.com>
2025-10-16 16:40:00 +0900

    Implement decoding logic in main.cpp
    Added logic to populate pointerArray and decode ASCII values.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
 
 int main() {
     int codeArray[48] = {16, 6, 19, 24, 14, 11, 18, 12, 15, 17, 2, 13, 22, 7, 10, 3, 23, 5, 20, 8, 9, 1, 4, 21, 116, 108, 32, 33, 104, 119, 101, 105, 32, 104, 111, 116, 97, 117, 32, 111, 109, 32, 101, 99, 107, 71, 100, 120};
@@ -15,5 +16,18 @@ int main() {
     //     Iterate index of pointerArray i from 0 to n-1.
     //     For each i, move forward by n elements to reach the paired ASCII code in the second half of codeArray.
     //     Convert that ASCII value to a character and print it.
-
+    for (int i = 1; i < n + 1; i++){
+        for (int j = 0; j < n; j++){
+            if (codeArray[j] == i){
+                pointerArray[i - 1] = &codeArray[j];
+                break;
+            }
+        }
+    } 
+    for (int i = 0 ; i < n; i++){
+        int ASCII_value = *(pointerArray[i] + n);
+        cout << static_cast<char>(ASCII_value);
+    }
+    cout << endl;
+    return 0;
 }
